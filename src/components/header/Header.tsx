6ddad1d7ea1ef2b82cb56b99566fb7da41396113
ChadsLaptop <chadjcampbell@gmail.com>
2023-04-01 19:24:30 -0400

    add react icons dep and a simple header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,3 +1,26 @@
+import { BsLinkedin, BsGithub } from "react-icons/bs";
+import "./header.css";
+
 export const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header>
+      <h2>CJC Dev Portfolio</h2>
+      <div>
+        <a
+          href="https://www.linkedin.com/in/chad-campbell-b6b59693/"
+          target="_blank"
+          aria-label="linked in new tab"
+        >
+          <BsGithub size={40} />
+        </a>
+        <a
+          href="https://github.com/chadjcampbell"
+          target="_blank"
+          aria-label="github new tab"
+        >
+          <BsLinkedin size={40} />
+        </a>
+      </div>
+    </header>
+  );
 };
