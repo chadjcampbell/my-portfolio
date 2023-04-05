ecf4b88af8f14df280d2d8ebab9b3851b3cc7baa
ChadsLaptop <chadjcampbell@gmail.com>
2023-04-05 17:22:06 -0400

    fill out skill section and add framer motion

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,29 +1,81 @@
 import "./skills.css";
+import { motion } from "framer-motion";
 
 const skillArray = [
-  "devicon-html5-plain colored",
-  "devicon-css3-plain colored",
-  "devicon-javascript-plain colored",
-  "devicon-typescript-plain colored",
-  "devicon-react-original colored",
-  "devicon-firebase-plain colored",
-  "devicon-sqlite-plain colored",
-  "devicon-nodejs-plain colored",
-  "devicon-git-plain colored",
-  "devicon-c-plain colored",
-  "devicon-python-plain colored",
-  "devicon-flask-original colored",
+  [
+    "devicon-html5-plain colored",
+    "HTML",
+    "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  ],
+  ["devicon-css3-plain colored", "CSS", "https://css-tricks.com/"],
+  [
+    "devicon-javascript-plain colored",
+    "Javascript",
+    "https://javascript.info/",
+  ],
+  [
+    "devicon-typescript-plain colored",
+    "Typescript",
+    "https://www.typescriptlang.org/",
+  ],
+  ["devicon-react-original colored", "React", "https://react.dev/"],
+  [
+    "devicon-firebase-plain colored",
+    "Firebase",
+    "https://firebase.google.com/",
+  ],
+  ["devicon-sqlite-plain colored", "SQLite", "https://sqlite.org/index.html"],
+  ["devicon-nodejs-plain colored", "NodeJS", "https://nodejs.org/en"],
+  ["devicon-git-plain colored", "Git", "https://git-scm.com/"],
+  ["devicon-c-plain colored", "C", "https://www.learn-c.org/"],
+  ["devicon-python-plain colored", "Python", "https://www.learnpython.org/"],
+  [
+    "devicon-flask-original colored",
+    "Flask",
+    "https://flask.palletsprojects.com/en/2.2.x/",
+  ],
 ];
 
 export const Skills = () => {
+  const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1,
+    },
+  };
+
   return (
     <section className="skills">
       <h2 className="title">Skills</h2>
-      <div className="skill-container">
+
+      <motion.ul
+        className="container skill-container"
+        variants={container}
+        initial="hidden"
+        animate="visible"
+      >
         {skillArray.map((skill) => (
-          <i className={`skill-icon ${skill}`}></i>
+          <motion.li key={skill[0]} className="item each-skill" variants={item}>
+            <a href={skill[2]} target="_blank">
+              <i className={`skill-icon ${skill[0]}`}></i>
+            </a>
+            <p>{skill[1]}</p>
+          </motion.li>
         ))}
-      </div>
+      </motion.ul>
     </section>
   );
 };
