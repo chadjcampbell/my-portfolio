ae8559347f95a8d6e6c1b0620c8c7a24bea01dbe
Chad <chadjcampbell@gmail.com>
2023-04-11 14:23:34 -0400

    extract skillArray and import

---
+++ src/components/skills/skillArray.ts
@@ -0,0 +1,65 @@
+export const skillArray = [
+  {
+    class: "devicon-html5-plain colored",
+    name: "HTML",
+    link: "https://developer.mozilla.org/en-US/docs/Web/HTML",
+  },
+  {
+    class: "devicon-css3-plain colored",
+    name: "CSS",
+    link: "https://css-tricks.com/",
+  },
+
+  {
+    class: "devicon-javascript-plain colored",
+    name: "Javascript",
+    link: "https://javascript.info/",
+  },
+  {
+    class: "devicon-typescript-plain colored",
+    name: "Typescript",
+    link: "https://www.typescriptlang.org/",
+  },
+  {
+    class: "devicon-react-original colored",
+    name: "React",
+    link: "https://react.dev/",
+  },
+
+  {
+    class: "devicon-firebase-plain colored",
+    name: "Firebase",
+    link: "https://firebase.google.com/",
+  },
+  {
+    class: "devicon-sqlite-plain colored",
+    name: "SQLite",
+    link: "https://sqlite.org/index.html",
+  },
+  {
+    class: "devicon-nodejs-plain colored",
+    name: "NodeJS",
+    link: "https://nodejs.org/en",
+  },
+  {
+    class: "devicon-git-plain colored",
+    name: "Git",
+    link: "https://git-scm.com/",
+  },
+  {
+    class: "devicon-c-plain colored",
+    name: "C",
+    link: "https://www.learn-c.org/",
+  },
+  {
+    class: "devicon-python-plain colored",
+    name: "Python",
+    link: "https://www.learnpython.org/",
+  },
+
+  {
+    class: "devicon-flask-original colored",
+    name: "Flask",
+    link: "https://flask.palletsprojects.com/en/2.2.x/",
+  },
+];
