81884c3f5904c99002bd91177f712bef2d48bf9a
Chad <chadjcampbell@gmail.com>
2023-04-19 18:05:07 -0400

    start animations on projects

--- src/components/projects/Projects.tsx
+++ src/components/projects/Projects.tsx
@@ -1,6 +1,8 @@
 import { BsGithub, BsTvFill } from "react-icons/bs";
 import { projectArray } from "./projectArray";
 import styles from "./projects.module.css";
+import { useState } from "react";
+import { motion } from "framer-motion";
 
 export type Project = {
   panel: string;
@@ -14,19 +16,36 @@ export type Project = {
 };
 
 export const Projects = () => {
+  const [projectsView, setProjectsView] = useState<Project[]>(projectArray);
+
+  const setActive = (name: string) => {
+    const newProjects = projectsView.map((project: Project) => {
+      if (project.name === name) {
+        return { ...project, hidden: false };
+      } else {
+        return { ...project, hidden: true };
+      }
+    });
+    setProjectsView(newProjects);
+  };
+
   return (
     <section className={styles.projects}>
       <h2>Projects</h2>
       <div className={styles.wrapper}>
         <div className={styles.accordion}>
-          {projectArray.map((project: Project) => (
-            <div key={project.name} className={styles["accordion-panel"]}>
+          {projectsView.map((project: Project) => (
+            <motion.div
+              key={project.name}
+              className={styles["accordion-panel"]}
+            >
               <div className={styles["card-header"]}>
                 <h3 id={project.panel + "-heading"}>
                   <button
                     className={styles["accordion-trigger"]}
                     aria-controls={project.panel + "-content"}
                     aria-expanded={!project.hidden}
+                    onClick={() => setActive(project.name)}
                   >
                     <span
                       className={styles["project-title"]}
@@ -42,16 +61,31 @@ export const Projects = () => {
                     />
                   </button>
                 </h3>
-                <div className={styles["project-links"]}>
-                  <a href={project.code} target="_blank" aria-label="linked-in">
-                    <BsGithub size="1.5rem" />
-                    Code
-                  </a>
-                  <a href={project.preview} target="_blank" aria-label="github">
-                    <BsTvFill size="1.75rem" />
-                    Live
-                  </a>
-                </div>
+                {!project.hidden && (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1, delay: 0.5 }}
+                    className={styles["project-links"]}
+                  >
+                    <a
+                      href={project.code}
+                      target="_blank"
+                      aria-label="linked-in"
+                    >
+                      <BsGithub size="1.5rem" />
+                      Code
+                    </a>
+                    <a
+                      href={project.preview}
+                      target="_blank"
+                      aria-label="github"
+                    >
+                      <BsTvFill size="1.75rem" />
+                      Live
+                    </a>
+                  </motion.div>
+                )}
               </div>
               <div
                 className={styles["accordion-content"]}
@@ -60,16 +94,23 @@ export const Projects = () => {
                 aria-hidden={project.hidden}
                 role="region"
               >
-                <div className={styles.description}>
-                  <p>{project.description}</p>
-                </div>
+                {!project.hidden && (
+                  <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1, delay: 0.5 }}
+                    className={styles.description}
+                  >
+                    <p>{project.description}</p>
+                  </motion.div>
+                )}
                 <img
                   className={styles["accordion-image"]}
                   src={project.background}
                   alt={project.name}
                 />
               </div>
-            </div>
+            </motion.div>
           ))}
         </div>
       </div>
