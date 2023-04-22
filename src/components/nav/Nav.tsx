d868e47ac0505f132c6d4733f4c502229e446ac4
Chad <chadjcampbell@gmail.com>
2023-04-22 18:26:18 -0400

    work on responsive img and navbar

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -7,15 +7,19 @@ export const Nav = () => {
     <nav className={styles.navbar}>
       <a href="#">
         <AiOutlineHome />
+        <p>Home</p>
       </a>
       <a href="#skills">
         <VscTools />
+        <p>Skills</p>
       </a>
       <a href="#projects">
         <AiOutlineDesktop />
+        <p>Projects</p>
       </a>
       <a href="#contact">
         <AiOutlineMail />
+        <p>Contact</p>
       </a>
     </nav>
   );
