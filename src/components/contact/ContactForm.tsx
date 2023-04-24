230c0502a1b5949136b17d61bfbbb54050b5acdf
Chad <chadjcampbell@gmail.com>
2023-04-24 17:57:17 -0400

    add toast message to contact form

--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -1,6 +1,8 @@
 import styles from "./contact.module.css";
 import { FormEvent, useRef } from "react";
 import emailjs from "@emailjs/browser";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 const ContactForm = () => {
   const form = useRef<HTMLFormElement>(null);
@@ -18,40 +20,60 @@ const ContactForm = () => {
         )
         .then(
           (result) => {
-            toast({
-              title: "Message Sent!",
-              description: "Thanks for reaching out.",
-              status: "success",
-              duration: 9000,
-              isClosable: true,
+            toast.success("Message sent, thank you for reaching out!", {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
             });
             const formTarget = e.target as HTMLFormElement;
             formTarget.reset();
           },
           (error) => {
             console.error(error.text);
-            toast({
-              title: "Message Failed!",
-              description: "Trolls may have locked up my mailJS account.",
-              status: "error",
-              duration: 9000,
-              isClosable: true,
+            toast.error("Message failed, please try again.", {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "dark",
             });
           }
         );
   };
 
   return (
-    <form ref={form} onSubmit={sendEmail} className={styles.form}>
-      <h4></h4>
-      <label htmlFor="name">Name</label>
-      <input type="text" id="name" name="from_name" />
-      <label htmlFor="email">Email</label>
-      <input type="text" id="email" name="from_email" />
-      <label htmlFor="subject">Message</label>
-      <textarea id="message" name="message"></textarea>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <form ref={form} onSubmit={sendEmail} className={styles.form}>
+        <h4></h4>
+        <label htmlFor="name">Name</label>
+        <input type="text" id="name" name="from_name" required />
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" name="from_email" required />
+        <label htmlFor="subject">Message</label>
+        <textarea id="message" name="message" required />
+        <button type="submit">Submit</button>
+      </form>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </>
   );
 };
 
