a65a93005088136df6a2b4f4eb3a8e463b374dd2
Chad-JET <chadjcampbell@gmail.com>
2023-05-05 14:59:35 -0400

    add a loading spinner to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
+import { useState, useEffect } from "react";
 import "./App.css";
 import { About } from "./components/about/About";
 import { Contact } from "./components/contact/Contact";
 import { Nav } from "./components/nav/Nav";
 import { Projects } from "./components/projects/Projects";
 import { Skills } from "./components/skills/Skills";
+import Loading from "./Loading";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const onPageLoad = () => {
+      setLoading(false);
+    };
+
+    // Check if the page has already loaded
+    if (document.readyState === "complete") {
+      onPageLoad();
+    } else {
+      window.addEventListener("load", onPageLoad);
+      // Remove the event listener when component unmounts
+      return () => window.removeEventListener("load", onPageLoad);
+    }
+  }, []);
+
   return (
     <div className="App">
+      {loading && <Loading />}
       <main>
         <Nav />
         <About />
